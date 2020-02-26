6ad3b7b11795beb728fd0a6cf99c27ceb9fe2994
Tyler Kummer <tylerkummer@protonmail.com>
2020-02-26 17:26:52 -0600

    Moved App function to an App.js file

--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -1,35 +1,5 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import Teams from "./Team/Teams";
-import TeamForm from "./Team/TeamForm"
+import App from "./App";
 
-function App() {
-    const [teams, setTeams] = useState([{
-        id: 1,
-        name: "Tyler Kummer",
-        email: "tylerkummer96@gmail.com",
-        role: "Student"
-    }]);
-
-    const addNewTeam = team => {
-        const newTeam = {
-            id: Date.now(),
-            name: team.name,
-            email: team.email,
-            role: team.role
-        };
-        setTeams([...teams, newTeam]);
-    }
-
-    return (
-        <div className="App" style={{width: "25%", margin: "0 auto"}}>
-            <h1>My Team</h1>
-            <TeamForm addNewTeam={addNewTeam}/>
-            <Teams teams={teams} />
-        </div>
-    );
-}
-
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
